93f48326ee98990c9a77d909c2ec254fecb086c9
willianzk <128807236+willianzk@users.noreply.github.com>
2024-05-02 12:17:21 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,19 @@
+const preço= "este preço é"
+let Preço= 80
+var este= (preço+Preço+8)
+
+
+
+const preço= "este preço é"
+let Preço= "80"
+var este= (preço+Preço+8)
+//este preço é 80
+
+
+const preço= este preço é
+//dará erro
+
+//operações 
+//(adição, subtração, multiplicação e divisão)
+//comparação
+//(==, >=, <=, ==, <,>)
